491a4ce38eb5514d401ebaa485bb7d03d3a60e91
Aliazhari <Aliazhari@hotmail.com>
2019-01-23 12:49:24 -0500

    creating index.php, /view/header, view/footer.php, view/index_view.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *
+ * Author: Ali Azhari
+ * File:   index.php
+ *
+ * 2018
+ */
+
+ ?>
+
+ <?php
+
+// Show the header and main body and foother of the this page
+include 'view/header.php';
+include 'view/index_view.php';
+include 'view/footer.php'; 
+
+?>
---
+++ view/footer.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *
+ * Author: Ali Azhari
+ * File:   view/footer.php
+ *
+ * 2018
+ */
+
+ ?>
+
+</body>
+</html>
